a34a62af55ab0c181bdc7b53493a5ac1f366a051
wujingwen <buptmywjw@163.com>
2024-06-19 14:43:16 +0800

    framework

---
+++ code_repair_framework/config.py
@@ -0,0 +1,8 @@
+unity_query_name_lis = {"Using New() allocation in Update() method.": "new_allocation.ql",
+             "Instantiate/Destroy in Update() method":"instan_destroy_in_update.ql",
+               "Transform object of Rigidbody in Update() methods":"rigidbody_transform_in_update.ql"}
+cwe_query_name_lis = {"Constant condition":"/Bad Practices/Control-Flow/ConstantCondition.ql",
+                      "Container contents are never accessed":"/Likely Bugs/Collections/WriteOnlyContainer.ql",
+                      "Locking the 'this' object in a lock statement":"/Concurrency/LockThis.ql",
+                      "Potentially dangerous use of non-short-circuit logic":"/Likely Bugs/DangerousNonShortCircuitLogic.ql",
+                      "Redundant Select":"/Linq/RedundantSelect.ql"}
---
+++ code_repair_framework/sort_all_unity_error.py
@@ -0,0 +1,62 @@
+import pandas as pd
+import re
+import csv
+import json
+from splinter import Browser
+import requests
+import time
+import os
+import config
+
+def sort_result_cwe(path):
+    files = os.walk(path)
+    unity_lis = {"Using New() allocation in Update() method.":[], "Instantiate/Destroy in Update() method":[],
+               "Transform object of Rigidbody in Update() methods":[]}
+
+    for path, dir_lis, file_lis in files:
+        for file in file_lis:
+            with open(os.path.join(path, file)) as f:
+                reader = csv.reader(f)
+
+                for rows in reader:
+                    unity_err = {"err_name": None, "description": None, "position": None, "sink_position": None,
+                                 "query_name": None, "database_name": None}
+                    unity_err["err_name"] = rows[0]
+                    unity_err["description"] = rows[1]
+                    unity_err["position"] = rows[4] + ":" + rows[5] + ":" + rows[6] + ":" + rows[7] + ":" + rows[8]
+                    unity_err["query_name"] = config.unity_query_name_lis[unity_err["err_name"]]
+                    unity_err["database_name"] = str(file).split("_unity")[0]
+
+                    if unity_err["err_name"] == "Instantiate/Destroy in Update() method":
+                        mid = rows[3].split("relative://")[1]
+                        unity_err["sink_position"] = str(mid).split('"')[0]
+                        print(unity_err["sink_position"])
+
+                    unity_lis[unity_err["err_name"]].append(unity_err)
+
+    with open("all_sort_unity.json", 'w') as f1:
+        f1.write(json.dumps(unity_lis))
+
+def count_cwe_num(file):
+    with open(file, 'r') as f:
+        lis = json.load(f)
+        cwe_len = {}
+        for cwe in lis:
+            name = cwe
+            length = len(lis[cwe])
+            cwe_len[name] = length
+
+    with open("total_cwe_num.json", 'w') as f1:
+        f1.write(json.dumps(cwe_len))
+    print("work done!")
+
+if __name__ == "__main__":
+    path = "result_unity"
+    file = "all_sort_cwe.json"
+    #file1 = "./all_sort_cwe_detailed.json"
+
+    sort_result_cwe(path)
+    #count_cwe_num(file1)
+
+
+
